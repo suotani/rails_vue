3041f65778f6b11beabca5338f7a47953d8aef2e
uotani <s.uotani.zetakansu@gmail.com>
2019-03-19 02:19:57 +0000

    フィルター、更新を実装 ただし、更新してもフィルタがうまく動かない。更新前のデータでフィルタがかかる

--- app/javascript/packs/enemy.js
+++ app/javascript/packs/enemy.js
@@ -1,22 +1,42 @@
 import Vue from 'vue'
 import axios from 'axios';
-var app = new Vue({
+window.app = new Vue({
   el: "#app",
   data: {
     enemies: [],
     enemyInfo: {},
-    show: false
+    show: false,
+    loading: true,
+    minHp: 0
   },
   created: function(){
       axios.get('/enemies/list')
       .then(res => {
           this.enemies = res.data
+          this.loading = false
       });
   },
+  computed: {
+      fillterd: function(){
+          return this.enemies.filter(function(el){
+              return el.hp >= this.minHp
+          }, this)
+      }
+  },
   methods: {
-      showDetail: function(index){
-          this.enemyInfo = this.enemies[index]
+      showDetail: function(enemy){
+          this.enemyInfo = Object.assign({}, enemy);
           this.show = true
+      },
+      
+      sortBy: function(key){
+          var list = this.enemies
+          this.enemies = list.sort(function(a,b){ return (a[key] > b[key]) ? -1 : 1})
+      },
+      
+      updateEnemy: function(id){
+          var index = this.enemies.findIndex(function(el){return el.id == id})
+          Vue.set(this.enemies, index, Object.assign({}, this.enemyInfo))
       }
   }
   
